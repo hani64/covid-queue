c3d988d5b1422d777df5dfd794d8d57d9b10438d
Hani Asim <haniasim@gmail.com>
2021-01-16 18:57:58 -0500

    cloud function bugs

--- functions/index.js
+++ functions/index.js
@@ -3,16 +3,12 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 
-// const express = require('express');
-// const app = express();
-
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions//
 
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+exports.helloWorld = functions.https.onCall((req, res) => {
+  return "test";
 });
 
 // //us-central1-hack-the-north-2021.cloudfunctions.net/helloWorld
---
+++ public/js/queue-me.js
@@ -0,0 +1,37 @@
+const formInputInfo = document.querySelectorAll('.info-input');
+const submitFormBtn = document.querySelector('#info-submit-btn');
+const loginBtn = document.querySelector('#login-btn');
+
+loginBtn.addEventListener('click', login);
+
+function login(e) {
+    e.preventDefault();
+
+    const helloWorld = firebase.functions().httpsCallable('helloWorld');
+    helloWorld().then(res => {
+        console.log(res.data);
+    });
+    loginBtn.disabled = 'disabled';
+}
+
+submitFormBtn.addEventListener('click', submitForm);
+
+// submitForm(e) collects all information from input elements in the form, storing it in
+//    an array. Order of information is determined by input element order in HTML file.
+function submitForm(e) {
+    e.preventDefault();
+
+    let allInputInfo = [];
+    for (const input of formInputInfo) {
+        allInputInfo.push(input.value);
+    }
+
+    let p = document.createElement('p');
+    
+    p.textContent += allInputInfo.join(' ');
+    document.querySelector(".info-form").appendChild(p);
+
+    submitFormBtn.disabled = 'disabled';
+}
+
+
